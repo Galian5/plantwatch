370c65d3de10d8cc08f3751a93635ccda10c07a0
Damian Podlejski <damian.podlejski@airhelp.com>
2022-02-13 17:53:14 +0100

    Update navbar. Add authenticated devices controller

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,19 +1,42 @@
 class DevicesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @devices = Device.all
   end
 
   def show
+    @device = Device.find(params[:id])
   end
 
   def new
+    @device = Device.new
   end
 
   def create
+    @device = Device.new(device_params)
+
+    if @device.save
+      redirect_to @device
+    else
+      render 'new'
+    end
   end
 
   def update
+    @device = Device.find(params[:id])
   end
 
   def destroy
+    @device = Device.find(params[:id])
+    @device.destroy
+
+    redirect_to devices_path
+  end
+
+  private
+
+  def device_params
+    params.require(:device).permit(:name, :user_id)
   end
 end
