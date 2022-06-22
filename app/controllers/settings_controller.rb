57428bbabdc8cedbd1b44372258d5656963651f1
Damian Podlejski <damian.podlejski@airhelp.com>
2022-06-22 23:19:59 +0200

    Add creating setting for plant.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,18 +1,50 @@
 class SettingsController < ApplicationController
 
   def show
-
+    @plant = Plant.find(params[:plant_id])
+    @setting = @plant.setting
   end
 
   def new
+    @setting = Setting.new
   end
 
   def create
+    @plant = Plant.find(params[:plant_id])
+    @setting = Setting.new(setting_params)
+    if @setting.save
+      redirect_to plants_path(@setting.plant_id)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
   end
 
+  def edit
+    @plant = Plant.find(params[:plant_id])
+    # @plant = plant.setting.update(measurement_params)
+  end
+
   def destroy
+    @setting = Setting.find(params[:id])
+    @setting.destroy
+
+    redirect_to plants_path(@setting.plant_id)
+  end
+
+  private
+
+  def setting_params
+    params.require(:setting).permit(:single_watering_amount,
+                                    :min_air_humidity,
+                                    :max_air_humidity,
+                                    :min_soil_moisture,
+                                    :max_soil_moisture,
+                                    :min_temperature,
+                                    :max_temperature,
+                                    :max_insolation,
+                                    :min_water_amount).merge(plant_id: params[:plant_id])
   end
 end
