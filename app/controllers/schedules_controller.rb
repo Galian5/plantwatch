0a8a42355d063004367c68bd80e9dd736c282898
Damian Podlejski <damian.podlejski@airhelp.com>
2022-07-07 14:25:53 +0200

    Add schedules views and controller methods

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,26 +1,37 @@
 class SchedulesController < ApplicationController
 
   def show
+    @plant = Plant.find(params[:plant_id])
+    @schedule = @plant.schedule
   end
 
   def new
+    @schedule = Schedule.new
   end
 
   def create
     @plant = Plant.find(params[:plant_id])
-    @schedule = @plant.schedule.create(schedule_params)
-    redirect_to plant_path(@plant)
+    # @schedule = @plant.schedule.create(schedule_params)
+    @schedule = Schedule.new(schedule_params)
+    p schedule_params
+
+    if @schedule.save
+
+      redirect_to plants_path(@schedule.plant_id)
+    else
+      p @schedule
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def update
-  end
-
-  def destroy
+  def edit
+    @plant = Plant.find(params[:plant_id])
+    @schedule = Schedule.find(params[:id])
   end
 
   private
 
   def schedule_params
-    params.require(schedule).permit(:watering, :interval)
+    params.require(:schedule).permit(:watering, :interval).merge(plant_id: params[:plant_id])
   end
 end
